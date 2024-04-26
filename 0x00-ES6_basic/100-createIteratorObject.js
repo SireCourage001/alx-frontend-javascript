46eee407b0d70c7c872b44dc8417333710c61b52
SireCourage001 <sirecourage@gmail.com>
2024-04-26 10:11:02 +0000

    edited_task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,16 +1,16 @@
 export default function createIteratorObject(report) {
-    const iterator = [];
-  
-    if (report) {
-      for (const department in report.allEmployees) {
-        if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
-          const employees = report.allEmployees[department];
-          for (const employee of employees) {
-            iterator.push(employee);
-          }
+  const iterator = [];
+
+  if (report) {
+    for (const department in report.allEmployees) {
+      if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+        const employees = report.allEmployees[department];
+        for (const employee of employees) {
+          iterator.push(employee);
         }
       }
     }
-  
-    return iterator;
   }
+
+  return iterator;
+}
