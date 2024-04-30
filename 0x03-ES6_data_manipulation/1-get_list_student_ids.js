a972eda1ca573090f66b4229698f5e22853fcbe6
SireCourage001 <sirecourage@gmail.com>
2024-04-30 09:41:56 +0000

    checked

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
+  if (Array.isArray(students)) {
     return students.map((student) => student.id);
   }
   return [];
