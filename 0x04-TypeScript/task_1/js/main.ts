24a8d45826d38ddc2f4b9d7fb65305f0371b4abb
SireCourage001 <134224615+SireCourage001@users.noreply.github.com>
2024-05-02 13:58:24 +0100

    updated

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,7 +2,7 @@ export interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
+    yearsOfExperience: number;
     location: string;
     [index:string]: any;
   }
